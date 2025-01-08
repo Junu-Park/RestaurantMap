1c6a079929672c951c7e4a9e4ee23c77a7b7e00d
Park Junwoo <ays99111@naver.com>
2025-01-08 17:40:20 +0900

    [feat] 프로젝트 파일 추가 및 구성

--- RestaurantMap/RestaurantViewController.swift
+++ RestaurantMap/RestaurantViewController.swift
@@ -1,14 +1,17 @@
 //
-//  ViewController.swift
+//  RestaurantViewController.swift
 //  RestaurantMap
 //
 //  Created by 박준우 on 1/8/25.
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
+class RestaurantViewController: UIViewController {
 
+    @IBOutlet weak var mapView: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
